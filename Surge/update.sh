a334cd326a12a10409d38d6cd970e8e5ababc276
WangWei <w7years@gmail.com>
2020-09-21 10:52:36 +0800

    update

---
+++ Surge/update.sh
@@ -0,0 +1,13 @@
+## reject
+curl https://ruleset.dev/surge3_reject > ./rule-set/reject/surge3_reject_1
+curl https://ruleset.dev/surge3_reject-tinygif > ./rule-set/reject/surge3_reject-tinygif
+ 
+## domestic
+curl https://ruleset.dev/surge3_netease_music > ./rule-set/domestic/surge3_netease_music
+curl https://ruleset.dev/surge3_domestic > ./rule-set/domestic/surge3_domestic
+
+## proxy
+curl https://ruleset.dev/surge3_stream_services > ./rule-set/proxy/surge3_stream_services
+curl https://ruleset.dev/surge3_global_plus > ./rule-set/proxy/surge3_global_plus
+curl https://ruleset.dev/surge3_global > ./rule-set/proxy/surge3_global
+curl https://ruleset.dev/surge3_apple_services > ./rule-set/proxy/surge3_apple_services
